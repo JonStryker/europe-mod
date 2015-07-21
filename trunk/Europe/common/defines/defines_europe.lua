49963d54957db4f071a53ee20ec2744a70a1875f
JonStryker <JonStryker@0627266c-4687-4182-a5e9-6e750a9bd593>
2015-07-21 07:58:46 +0000

    defines removed (to be fixed)

---
+++ trunk/Europe/common/defines/defines_europe.lua
@@ -0,0 +1,24 @@
+-- Format for overwriting define values:
+--
+-- NDefines.NDiplomacy.DEMESNE_BASE_MAX_SIZE = 2.0
+
+
+-- TODO: REINSTATE THE FOLLOWING
+
+-- DEMAND_RELIGIOUS_CONVERSION_INTERACTION_ENABLED = 0
+-- PORTRAIT_MID_AGE_THRESHOLD = 40,				-- Middle age switch for character portraits
+--	PORTRAIT_OLD_AGE_THRESHOLD = 60,				-- Old age switch for character portraits
+--	AGE_OF_ADULTHOOD_MALE = 18,						-- Male can rule at this age. 
+--	AGE_OF_ADULTHOOD_FEMALE = 18,					-- Female can rule at this age.
+--	AGE_OF_MARRIAGE_MALE = 20,						-- Male allowed to marry at this age.
+--	AGE_OF_MARRIAGE_FEMALE = 20,					-- Female allowed to marry at this age.
+--	AGE_YOUNG = 20,									-- Non-child characters below this age have the 'teen' static modifier applied
+--	AGE_ADULT = 35,									-- Non-child characters below this age have the 'young' static modifier applied
+--	AGE_OLD = 60,									-- Non-child characters below this age have the 'adult' static modifier applied
+--	AGE_VERY_OLD = 90,
+-- NATURAL_DEATH_CHANCE_AGE_50 = 75,				-- Natural deaths per decade out of 10000 people: Age 50-59
+--	NATURAL_DEATH_CHANCE_AGE_60 = 150,				-- Natural deaths per decade out of 10000 people: Age 60-69
+--	NATURAL_DEATH_CHANCE_AGE_70 = 700,				-- Natural deaths per decade out of 10000 people: Age 70-79
+--	NATURAL_DEATH_CHANCE_AGE_80 = 1800,				-- Natural deaths per decade out of 10000 people: Age 80-90
+--	NATURAL_DEATH_CHANCE_AGE_90 = 5000,				-- Natural deaths per decade out of 10000 people: Age 90-99
+--	NATURAL_DEATH_CHANCE_AGE_100 = 7500,			
