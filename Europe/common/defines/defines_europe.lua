3611faf075351806e31be28245e58b417fe79e54
JonStryker <JonStryker@users.noreply.github.com>
2015-09-10 22:36:15 +0000

    title assignments

--- Europe/common/defines/defines_europe.lua
+++ Europe/common/defines/defines_europe.lua
@@ -3,6 +3,22 @@
 -- NDefines.NDiplomacy.DEMESNE_BASE_MAX_SIZE = 2.0
 
 NDefines.NTechnology.DONT_EXECUTE_TECH_BEFORE = 2000
+NDefines.NCharacter.NATURAL_DEATH_CHANCE_AGE_0 = 5					-- Natural deaths per decade out of 10000 people: Age 0-9
+NDefines.NCharacter.NATURAL_DEATH_CHANCE_AGE_10 = 1					-- Natural deaths per decade out of 10000 people: Age 10-19
+NDefines.NCharacter.NATURAL_DEATH_CHANCE_AGE_20 = 15				-- Natural deaths per decade out of 10000 people: Age 20-29
+NDefines.NCharacter.NATURAL_DEATH_CHANCE_AGE_30 = 20				-- Natural deaths per decade out of 10000 people: Age 30-39
+NDefines.NCharacter.NATURAL_DEATH_CHANCE_AGE_40 = 40				-- Natural deaths per decade out of 10000 people: Age 40-49
+NDefines.NCharacter.NATURAL_DEATH_CHANCE_AGE_50 = 75				-- Natural deaths per decade out of 10000 people: Age 50-59
+NDefines.NCharacter.NATURAL_DEATH_CHANCE_AGE_60 = 150				-- Natural deaths per decade out of 10000 people: Age 60-69
+NDefines.NCharacter.NATURAL_DEATH_CHANCE_AGE_70 = 700				-- Natural deaths per decade out of 10000 people: Age 70-79
+NDefines.NCharacter.NATURAL_DEATH_CHANCE_AGE_80 = 1800				-- Natural deaths per decade out of 10000 people: Age 80-90
+NDefines.NCharacter.NATURAL_DEATH_CHANCE_AGE_90 = 5000				-- Natural deaths per decade out of 10000 people: Age 90-99
+NDefines.NCharacter.NATURAL_DEATH_CHANCE_AGE_100 = 7500				-- Natural deaths per decade out of 10000 people: Age 100+
+NDefines.NCharacter.PORTRAIT_OLD_AGE_THRESHOLD = 60
+NDefines.NCharacter.AGE_OF_ADULTHOOD_MALE = 18 						-- Male can rule at this age. 
+NDefines.NCharacter.AGE_OF_ADULTHOOD_FEMALE = 18 					-- Female can rule at this age.
+NDefines.NCharacter.AGE_OF_MARRIAGE_MALE = 20 						-- Male allowed to marry at this age.
+NDefines.NCharacter.AGE_OF_MARRIAGE_FEMALE = 20						-- Female allowed to marry at this age.
 
 
 -- TODO: REINSTATE THE FOLLOWING
